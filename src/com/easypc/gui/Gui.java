06f2e3b2f0d2925547e12afe696737efb818e161
serpent90 <niklas.assmann@googlemail.com>
2012-04-15 22:08:54 +0200

    implemented function showList()

--- src/com/easypc/gui/Gui.java
+++ src/com/easypc/gui/Gui.java
@@ -2,9 +2,11 @@ package com.easypc.gui;
 
 import java.awt.Color;
 import java.io.File;
-import java.util.Arrays;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
 
 import com.easypc.analysis.CPUAnalysisC;
 import com.easypc.analysis.RAMAnalysisC;
@@ -27,6 +29,7 @@ public class Gui {
 	private GuiFrame guiFrame;
 	//The listview containing all Games
 	private JList gameList;	//TODO: handle the Input from the list correctly, a.k.a: Load the chosen Game and re-attach the gameCanvas after a game was chosen
+	private GameCanvas gameCanvas;
 	
 	/*----------------------------------------------------
 	 * Public Method Section. Shows the Methods directly available from other Classes:
@@ -39,6 +42,7 @@ public class Gui {
 	public Gui(Controller controller, CPUAnalysisC cpuAnalysisC, RAMAnalysisC ramAnalysisC, GameCanvas gamecanvas)
 	{
 		this.controller = controller;
+		this.gameCanvas = gamecanvas;
 		
 		guiFrame = new GuiFrame();
 		guiFrame.getContentPane().setBackground(Color.BLACK);
@@ -54,23 +58,62 @@ public class Gui {
 		ramAnalysisC.addMouseMotionListener(guiFrame);   
 		guiFrame.add(ramAnalysisC);
 		
-		gamecanvas.setBounds(385, 230, 416, 200);
-		gamecanvas.addMouseListener(guiFrame);
-		gamecanvas.addMouseMotionListener(guiFrame);   
-		guiFrame.add(gamecanvas);
+		gameCanvas.setBounds(385, 230, 416, 200);
+		gameCanvas.addMouseListener(guiFrame);
+		gameCanvas.addMouseMotionListener(guiFrame);   
+		guiFrame.add(gameCanvas);
+		
+		showList();
 	}
 	
 	/**
 	 * Replaces the gamecanvas and shows a JList instead containing all games from a folder
 	 */
 	private void showList()
-	{		
-		//TODO: mage the gamecanvas invisible and show a JList http://docs.oracle.com/javase/tutorial/uiswing/components/list.html
+	{
+		//TODO: Check showList() Implementation
+		// Directory path here
+		String path = "."; //TODO: where do we store the game files?
+		 
+		String files;
+		File folder = new File(path);
+		File[] listOfFiles = folder.listFiles(); 
+		DefaultListModel listmodel = new DefaultListModel();
+		
+		for (int i = 0; i < listOfFiles.length; i++) 
+		{
+		 if (listOfFiles[i].isFile()) 
+		 {
+		 files = listOfFiles[i].getName();
+		 listmodel.addElement(files);
+		 //System.out.println(files); //remove comment for debug output of the filelist
+		    }
+		}
+		
+		//make gameCanvas invisible (TODO: Pls check this - I created a private reference to the gamecanvas, is that ok?)
+		gameCanvas.setVisible(false);
+		
+		//create JList of games called gameList and add it to the guiFrame
+		gameList = new JList(listmodel);
+		gameList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		gameList.setSelectedIndex(0);
+		gameList.setVisibleRowCount(5);
+		gameList.setBounds(385, 230, 416, 200);
+		gameList.addMouseListener(guiFrame);
+		gameList.addMouseMotionListener(guiFrame);
+		gameList.setVisible(true);
+		guiFrame.add(gameList);
+		
+		//this one is necessary, dont know why...
+		guiFrame.repaint();
+		
+		
+		// mage the gamecanvas invisible and show a JList http://docs.oracle.com/javase/tutorial/uiswing/components/list.html
 		//	which contains the list array
 		//use the gameList Attribute from this class
 		//TIP: http://docs.oracle.com/javase/tutorial/uiswing/components/list.html
 		
-		//TODO: Use the code-snippet below from Fritz to display all files from a given folder
+		// Use the code-snippet below from Fritz to display all files from a given folder
 		//The dot "." will get you all the files in a folder, e.g. "bin/." should get you all files within the bin folder when used like below
 		//String[] entries = new File( "." ).list(); 
 		//System.out.println( Arrays.toString(entries) );
