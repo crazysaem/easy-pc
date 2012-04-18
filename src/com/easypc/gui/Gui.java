530bff426922ad1ec840b9ba843b34f0581c40dc
serpent90 <niklas.assmann@googlemail.com>
2012-04-18 18:45:24 +0200

    added comments, todos etc. to Gui.java and InputLWJGL.java

--- src/com/easypc/gui/Gui.java
+++ src/com/easypc/gui/Gui.java
@@ -25,6 +25,7 @@ import com.easypc.analysis.RAMAnalysisC;
 import com.easypc.backend.InputLWJGL;
 import com.easypc.chip8.GameCanvas;
 import com.easypc.controller.Controller;
+import com.easypc.controller._main;
 
 /**
  * Gui Master - puts the entire Gui together and manages it
@@ -40,17 +41,24 @@ public class Gui implements ImageButtonLabelCallBack {
 	// The controller will get information about the input from the user via the
 	// gui events in this class
 	private Controller controller;
+	
 	// the main gui frame
 	private GuiFrame guiFrame;
+	
 	// The listview containing all Games
-	private JList gameList;
+	private JList<String> gameList;
+	
 	private GameCanvas gameCanvas;
 	private InputLWJGL input;
+	
+	//reference to the fullscreen frame
 	private JFrame full;
+	
+	//JList Scrollpane
 	private JScrollPane scrollPane;
 
 	private ImageButtonLabel min, close;
-	
+
 	/*----------------------------------------------------
 	 * Public Method Section. Shows the Methods directly available from other Classes:
 	 *--------------------------------------------------*/
@@ -63,13 +71,15 @@ public class Gui implements ImageButtonLabelCallBack {
 	public Gui(Controller controller, CPUAnalysisC cpuAnalysisC,
 			RAMAnalysisC ramAnalysisC, GameCanvas gamecanvas, InputLWJGL input) {
 		this.controller = controller;
-		this.gameCanvas = gamecanvas;	
+		this.gameCanvas = gamecanvas;
 		this.input = input;
-		input.checkKeys();
+		
+		//runnning keylistener for the guiFrame
+		this.input.checkKeys();
 
 		guiFrame = new GuiFrame();
 		guiFrame.getContentPane().setBackground(Color.BLACK);
-		guiFrame.setVisible(true);	
+		guiFrame.setVisible(true);
 		guiFrame.addKeyListener(input.keylistener);
 
 		cpuAnalysisC.setBounds(409, 7, 777 - 409, 185 - 7);
@@ -82,60 +92,61 @@ public class Gui implements ImageButtonLabelCallBack {
 		ramAnalysisC.addMouseMotionListener(guiFrame);
 		guiFrame.add(ramAnalysisC);
 
-		gameCanvas.addKeyListener(input.keylistener);		
+		gameCanvas.addKeyListener(input.keylistener);
 		gameCanvas.setBounds(385, 230, 416, 200);
 		gameCanvas.addMouseListener(guiFrame);
 		gameCanvas.addMouseMotionListener(guiFrame);
-		guiFrame.add(gameCanvas);		
-		
+		guiFrame.add(gameCanvas);
+
 		showList();
 
-		close = new ImageButtonLabel(this, "src/resources/keys/close.png", "src/resources/keys/close_glow.png", 770, 190, 0.4f);	
+		close = new ImageButtonLabel(this, "src/resources/keys/close.png",
+				"src/resources/keys/close_glow.png", 770, 190, 0.4f);
 		guiFrame.add(close.getLabel());
-		
-		min = new ImageButtonLabel(this, "src/resources/keys/min.png", "src/resources/keys/min_glow.png", 730, 190, 0.4f);	
+
+		min = new ImageButtonLabel(this, "src/resources/keys/min.png",
+				"src/resources/keys/min_glow.png", 730, 190, 0.4f);
 		guiFrame.add(min.getLabel());
 	}
-	
+
 	@Override
 	public void ButtonCallBack(ImageButtonLabel pressedButton) {
-		if(pressedButton==close)
-		{	
+		if (pressedButton == close) {
 			fullscreen();
-			//System.exit(0);
-		} else if(pressedButton==min)
-		{
-			guiFrame.setState ( Frame.ICONIFIED );
+			// System.exit(0);
+		} else if (pressedButton == min) {
+			guiFrame.setState(Frame.ICONIFIED);
 		}
 	}
 
 	/**
 	 * Replaces the gamecanvas and shows a JList instead containing all games
-	 * from a folder
-	 * Replaces the gamecanvas and shows a JList instead containing all games from a folder
+	 * from a folder Replaces the gamecanvas and shows a JList instead
+	 * containing all games from a folder
 	 */
 	private void showList() {
 
-		DefaultListModel listmodel = controller.getRomList();
+		DefaultListModel<String> listmodel = controller.getRomList();
 
 		// make gameCanvas invisible
 		gameCanvas.setVisible(false);
 
 		// create JList of games called gameList and add it to the guiFrame
-		gameList = new JList(listmodel);
+		gameList = new JList<String>(listmodel);
 
-		
-		
 		MouseListener mouseListener = new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
 				if (e.getClickCount() == 2) {
 					int index = gameList.locationToIndex(e.getPoint());
+					
+					//TODO: I think this should be located in the controller
 					File game = new File("src/resources/games/"
 							+ gameList.getModel().getElementAt(index)
 									.toString());
-					// debug code
-					System.out.println("Loading "
-							+ gameList.getModel().getElementAt(index));
+					//
+					
+					if(_main.DEBUG)
+						System.out.println("Loading " + gameList.getModel().getElementAt(index));
 					controller.loadGame(game);
 					showGameCanvas();
 					try {
@@ -151,13 +162,15 @@ public class Gui implements ImageButtonLabelCallBack {
 		KeyListener keylistener = new KeyListener() {
 			public void keyPressed(KeyEvent e) {
 				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					
+					//TODO: refactoring - same as in MouseListener
 					File game = new File("src/resources/games/"
 							+ gameList.getSelectedValue().toString());
 					// debug code
-					System.out.println("Loading "
-							+ gameList.getModel().getElementAt(0));
+					if(_main.DEBUG)
+						System.out.println("Loading " + gameList.getModel().getElementAt(0));
 					controller.loadGame(game);
-					showGameCanvas();					
+					showGameCanvas();
 					try {
 						controller.playGame();
 					} catch (InterruptedException e1) {
@@ -179,7 +192,6 @@ public class Gui implements ImageButtonLabelCallBack {
 			}
 		};
 
-		
 		gameList.addKeyListener(keylistener);
 		gameList.addMouseListener(mouseListener);
 		gameList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -193,7 +205,7 @@ public class Gui implements ImageButtonLabelCallBack {
 		scrollPane = new JScrollPane(gameList);
 		scrollPane.setBounds(385, 230, 416, 200);
 		scrollPane.setVisible(true);
-		
+
 		guiFrame.add(scrollPane);
 		guiFrame.repaint();
 		gameList.requestFocusInWindow();
@@ -203,48 +215,49 @@ public class Gui implements ImageButtonLabelCallBack {
 	 * Removes the JList game list, and shows the gamecanvas again
 	 */
 	private void showGameCanvas() {
-		// setting the gamelist ( see function showList() ) invisible and the
-		// gameCanvas visible again
 		gameList.setVisible(false);
 		scrollPane.setVisible(false);
-		gameCanvas.setVisible(true);		
+		gameCanvas.setVisible(true);
 		gameCanvas.requestFocusInWindow();
 		guiFrame.repaint();
 	}
 
-	
-	private void fullscreen(){
+	/**
+	 * Creates a fullscreen JFrame with the gamecanvas on it
+	 */
+	private void fullscreen() {
 		GraphicsEnvironment ge = GraphicsEnvironment
-                .getLocalGraphicsEnvironment();
-        GraphicsDevice[] gs = ge.getScreenDevices();
-        for (int j = 0; j < gs.length; j++) {
-            GraphicsDevice gd = gs[j];
-            DisplayMode dm = gd.getDisplayMode();
-            full = new JFrame(gs[j].getDefaultConfiguration());
-            GraphicsConfiguration[] gcs = gd.getConfigurations();           
-            Canvas c = new Canvas(gd.getDefaultConfiguration());            
-            guiFrame.remove(gameCanvas);
-            c = gameCanvas;            
-            full.getContentPane().add(c);
-            full.setUndecorated(true);
-            System.out.println(gd.isFullScreenSupported());
-            if (gd.isDisplayChangeSupported()) {
-                gd.setDisplayMode(dm);
-            }
-            gd.setFullScreenWindow(full); 
-            c.requestFocusInWindow();
-        }
-       
+				.getLocalGraphicsEnvironment();
+		GraphicsDevice[] gs = ge.getScreenDevices();
+		for (int j = 0; j < gs.length; j++) {
+			GraphicsDevice gd = gs[j];
+			DisplayMode dm = gd.getDisplayMode();
+			full = new JFrame(gs[j].getDefaultConfiguration());
+			GraphicsConfiguration[] gcs = gd.getConfigurations();
+			Canvas c = new Canvas(gd.getDefaultConfiguration());
+			guiFrame.remove(gameCanvas);
+			c = gameCanvas;
+			full.getContentPane().add(c);
+			full.setUndecorated(true);
+			System.out.println(gd.isFullScreenSupported());
+			if (gd.isDisplayChangeSupported()) {
+				gd.setDisplayMode(dm);
+			}
+			gd.setFullScreenWindow(full);
+			c.requestFocusInWindow();
+		}
+
 	}
 
-	public void reset_fullscreen(){
+	/**
+	 * resets the fullscreen view to normal view again
+	 */
+	public void reset_fullscreen() {
 		full.getContentPane().removeAll();
 		full.setVisible(false);
 		gameCanvas.setBounds(385, 230, 416, 200);
 		guiFrame.add(gameCanvas);
 		showGameCanvas();
-		
-						
 	}
-	
+
 }
